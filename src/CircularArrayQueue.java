db68538249b18cdb0125e7604fe164740f08091d
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-09 20:50:58 +0000

    Fixed enqueue

--- src/CircularArrayQueue.java
+++ src/CircularArrayQueue.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class CircularArrayQueue implements MyQueue{
@@ -6,7 +7,7 @@ public class CircularArrayQueue implements MyQueue{
     private int head, tail, elementCount;
 
     public CircularArrayQueue() {
-        queue = new int[0];
+        queue = new int[1];
         head = tail = elementCount = 0;
     }
     @Override
@@ -17,9 +18,9 @@ public class CircularArrayQueue implements MyQueue{
     @Override
     public int dequeue() throws NoSuchElementException {
         int next = queue[head];
-        System.out.println("Front of queue: " + next);
+//        System.out.println("Front of queue: " + next);
         head++;
-        if (head > queue.length)
+        if (head == queue.length)
             head = 0;
         elementCount--;
         return next;
@@ -27,27 +28,29 @@ public class CircularArrayQueue implements MyQueue{
 
     @Override
     public int noItems() {
-        System.out.println("No. of elements: " + elementCount);
+//        System.out.println("No. of elements: " + elementCount);
         return elementCount;
     }
 
     @Override
     public void enqueue(int in) {
         if (getCapacityLeft() == 0) {
+//            System.out.println("No room in array");
             int[] oldArray = new int[queue.length];
             System.arraycopy(queue, 0, oldArray, 0, queue.length);
-            if (queue.length == 0)
-                queue = new int[1];
-            else
-                queue = new int[queue.length * 2];
+//            System.out.println("Previous array size: " + queue.length);
+            queue = new int[queue.length * 2];
             System.arraycopy(oldArray, 0, queue, 0, oldArray.length);
-        } else {
-            queue[tail] = in;
-            elementCount++;
-            tail++;
-            if (tail > queue.length)
-                tail = 0;
+//            System.out.println("New array size: " + queue.length);
+
         }
+//           System.out.println("Adding " + in + " to queue");
+        queue[tail] = in;
+        elementCount++;
+        tail++;
+        if (tail == queue.length)
+            tail = 0;
+//        System.out.println(Arrays.toString(queue));
     }
 
     public int getCapacityLeft() {
--- src/CircularArrayQueueTest.java
+++ src/CircularArrayQueueTest.java
@@ -77,8 +77,7 @@ public class CircularArrayQueueTest extends TestCase
         }
         for( int i = 0; i < r.length; ++i )
         {
-            assertEquals( "Dequeue returns wrong element", r[ i ],
-                    queue.dequeue() );
+            assertEquals( "Dequeue returns wrong element", r[ i ], queue.dequeue() );
             checkSize( r.length - i - 1, queue );
         }
         for( int i = 0; i < r.length; ++i )
@@ -107,8 +106,7 @@ public class CircularArrayQueueTest extends TestCase
         }
         for( int i = 0; i < r.length; ++i )
         {
-            assertEquals( "Dequeue returns wrong element", r[ i ],
-                    queue.dequeue() );
+            assertEquals( "Dequeue returns wrong element", r[ i ], queue.dequeue() );
             checkSize( r.length - i - 1, queue );
         }
         boolean throwsCorrectly = false;
